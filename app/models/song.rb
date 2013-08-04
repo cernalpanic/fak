ddd05c578fec937815c3daadcc4b6b02535de812
Todd Holdman <todbot@gmail.com>
2013-08-04 11:17:45 -0500

    fixed songs association bug

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,9 @@
 class Song < ActiveRecord::Base
 	attr_accessible :title, :album_id
 
-	belongs_to :albums
-	belongs_to :shows
-	belongs_to :artists
+	belongs_to :album
+	belongs_to :show
+	belongs_to :artis
 	has_many :links
 
 	validates_uniqueness_of :title, :case_sensitive => false
