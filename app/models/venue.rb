e9d606cfc8bee2ee5529e67e84878b1ac458732c
Todd Holdman <todbot@gmail.com>
2013-09-11 21:22:04 -0500

    cleaning up removed files

--- app/models/venue.rb
+++
@@ -1,8 +0,0 @@
-class Venue < ActiveRecord::Base
-  attr_accessible :city, :country, :description, :name, :state
-
-  has_many :shows
-  accepts_nested_attributes_for :shows
-
-	validates :name, :presence => {:message => 'Name cannot be blank, Venue not saved; even a close but wrong name is better than no name. We can always edit it later.'}
-end
